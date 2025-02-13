7f0ef76a56dbe772aaa095e1c74a19552b5ba1e5
n0x29a <n0x29a@example.com>
2025-02-13 11:00:11 +0100

    use_idf fix

--- src/steps/GenericElementsStep.jsx
+++ src/steps/GenericElementsStep.jsx
@@ -16,6 +16,13 @@ const GenericElementsStep = function ({
   const value = stepData || {};
 
   let isStepCompleted = true;
+
+  console.log('Step data:', {
+    stepName,
+    value,
+    config
+  });
+
   let totalElements = config.elements && config.elements.length;
 
   const renderedElements =
@@ -29,6 +36,13 @@ const GenericElementsStep = function ({
       const elementData = value[element.name];
 
       const isElementRequired = elementConfig.required === true;
+
+      console.log('Element check:', {
+        name: element.name,
+        data: elementData,
+        required: isElementRequired
+      });
+
       let isElementCompleted = checkCompleted(elementData, isElementRequired);
 
       isStepCompleted = isStepCompleted && isElementCompleted;
